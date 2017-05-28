4b3db41c710218ec8c81b03b79fa50ca9f15feef
Valdir Luiz Hofer Arnhold <Valdir Luiz Hofer Arnhold>
2017-05-28 12:04:49 -0300

    fazer alterações na produção atribstat

--- syntacticTree/AtribNode.java
+++ syntacticTree/AtribNode.java
@@ -6,10 +6,12 @@ import parser.*;
 public class AtribNode extends StatementNode {
     public ExpreNode expr1;
     public ExpreNode expr2;
+    public Token t2;
 
-    public AtribNode(Token t, ExpreNode e1, ExpreNode e2) {
-        super(t);
+    public AtribNode(Token t1, ExpreNode e1, ExpreNode e2, Token t2) {
+        super(t1);
         expr1 = e1;
         expr2 = e2;
+        this.t2 = t2;
     }
 }
